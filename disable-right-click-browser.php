a85410d252768a98bf239969ed1f66a4f1f10e5f
vaibhav <vivgangs@gmail.com>
2024-01-07 17:59:41 +0530

    Add script from admin and load on the page

--- disable-right-click-browser.php
+++ disable-right-click-browser.php
@@ -1,16 +1,31 @@
 <?php 
 /*
 * Plugin Name:Disable Right Click
-* Author:Vaibhav
+* Author:Vaibhav Gangrade
 * Version:1.0
 * Description:Disable right click of browser for preventing data threat.
 * Author URI:
+* Author URL:
 */
 
-if(!defined('ABSPATH')) exit;
+if(!defined('ABSPATH')) exit;//Prevent Direct Access
 if(!function_exists('DRC_disable_click')){
+
+	if (!function_exists('DRC_load_admin_js'))
+	{
+	    function DRC_load_admin_js()
+	    {
+	        //adding jquery
+	        wp_enqueue_script(array(
+	            'jquery'
+	        ));
+	    }
+	    add_action('wp_enqueue_scripts', 'DRC_load_admin_js');
+	
+	}
 	function DRC_disable_click(){ ?>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> -->
+		
 		<script type="text/javascript">
 		jQuery(document).ready(function() {
 		    jQuery("body").on("contextmenu",function(){
@@ -27,7 +42,5 @@ if(!function_exists('DRC_disable_click')){
 		}); 
 		</script>
 	<?php }
-	
-
 }
 add_action('init','DRC_disable_click');
