d0e61cd39b34c926282d93fed91b7e7668c5c085
Vaibhav Gangrade <45598521+Viv31@users.noreply.github.com>
2024-05-31 22:31:14 +0530

    Update disable-right-click-browser.php
    Added inbuilt jQuery library

--- disable-right-click-browser.php
+++ disable-right-click-browser.php
@@ -1,46 +1,67 @@
-<?php 
+<?php
 /*
-* Plugin Name:Disable Right Click
-* Author:Vaibhav Gangrade
-* Version:1.0
-* Description:Disable right click of browser for preventing data threat.
+* Plugin Name: Disable Right Click
+* Author: Vaibhav Gangrade
+* Version: 1.0
+* Description: Disable right click and prevent screenshot and print screen to protect content.
 * Author URI:
 * Author URL:
 */
 
-if(!defined('ABSPATH')) exit;//Prevent Direct Access
-if(!function_exists('DRC_disable_click')){
+if (!defined('ABSPATH')) exit; // Prevent Direct Access
 
-	if (!function_exists('DRC_load_admin_js'))
-	{
-	    function DRC_load_admin_js()
-	    {
-	        //adding jquery
-	        wp_enqueue_script(array(
-	            'jquery'
-	        ));
-	    }
-	    add_action('wp_enqueue_scripts', 'DRC_load_admin_js');
-	
-	}
-	function DRC_disable_click(){ ?>
-		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> -->
-		
-		<script type="text/javascript">
-		jQuery(document).ready(function() {
-		    jQuery("body").on("contextmenu",function(){
-		       return false;
-		    }); 
-
-		    jQuery(document).keydown(function (event) {
-		    if (event.keyCode == 123) { // Prevent F12
-		        return false;
-		    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-		        return false;
-		    }
-		});
-		}); 
-		</script>
-	<?php }
+if (!function_exists('DRC_load_admin_js')) {
+    function DRC_load_admin_js() {
+        // Enqueue jQuery
+        wp_enqueue_script('jquery');
+    }
+    add_action('wp_enqueue_scripts', 'DRC_load_admin_js');
 }
-add_action('init','DRC_disable_click');
+
+if (!function_exists('DRC_disable_click')) {
+    function DRC_disable_click() { ?>
+        <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            // Disable right-click
+           jQuery("body").on("contextmenu", function() {
+                return false;
+            });
+
+            // Disable specific key combinations
+            jQuery(document).keydown(function(event) {
+                if (event.keyCode == 123) { // Prevent F12
+                    return false;
+                } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+                    return false;
+                } else if (event.ctrlKey && event.keyCode == 85) { // Prevent Ctrl+U
+                    return false;
+                } else if (event.keyCode == 44) { // Prevent PrintScreen
+                    alert('Screenshots are disabled.');
+                    return false;
+                }
+            });
+
+            // Attempt to detect and prevent screenshot via clipboard
+            jQuery(window).blur(function() {
+                setTimeout(function() {
+                    try {
+                        let image = new Image();
+                        image.src = '';
+                        image.onerror = function() {
+                            console.log('Attempt to capture screenshot detected.');
+                            // You can add additional actions here, e.g., blur the content
+                            jQuery('body').css('visibility', 'hidden');
+                            setTimeout(function() {
+                                jQuery('body').css('visibility', 'visible');
+                            }, 2000);
+                        };
+                    } catch (e) {}
+                }, 200);
+            });
+        });
+        </script>
+    <?php }
+}
+
+add_action('wp_footer', 'DRC_disable_click');
+?>
